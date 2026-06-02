1c925ec81fb9c53efd76bb362d198ef15e1434d5
Maxuel Rodrigues <maxuel.rodrigues@sefaz.es.gov.br>
2026-06-02 09:11:13 -0300

    add get courses and update course endpoint

--- Controllers/CoursesController.cs
+++ Controllers/CoursesController.cs
@@ -1,6 +1,8 @@
 using CoursesApi.Data;
 using CoursesApi.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Slugify;
 
 namespace CoursesApi.Controllers
 {
@@ -9,7 +11,7 @@ namespace CoursesApi.Controllers
     public class CoursesController : ControllerBase
     {
         private AppDbContext _appDbContext;
-
+        
         public CoursesController(AppDbContext appDbContext)
         {
             _appDbContext = appDbContext;
@@ -21,6 +23,32 @@ namespace CoursesApi.Controllers
             _appDbContext.Courses.Add(course);
             await _appDbContext.SaveChangesAsync();
             return Created();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllCourses()
+        {
+            var courses = await _appDbContext.Courses.ToListAsync();
+            return Ok(courses);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateCourse(int id, Course course)
+        {
+            var helper = new SlugHelper();
+            var courseUpdated = await _appDbContext.Courses.FindAsync(id);
+            if (courseUpdated is null)
+                return NotFound();
+
+            courseUpdated.CategoryId = course.CategoryId;
+            courseUpdated.Name = course.Name;
+            courseUpdated.Slug = helper.GenerateSlug(course.Name);
+            courseUpdated.Description = course.Description;
+            courseUpdated.Duration = course.Duration;
+            courseUpdated.Price = course.Price;
+
+            await _appDbContext.SaveChangesAsync();
+            return NoContent();
         }
     }
 }
